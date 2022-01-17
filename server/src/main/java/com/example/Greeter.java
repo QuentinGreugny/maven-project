893e6439a3930cfe658266424dca7fb284ae124e
QuentinGreugny <90702474+QuentinGreugny@users.noreply.github.com>
2022-01-17 12:40:27 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,4 +1,5 @@
 package com.example;
+package-info.java;
 
 /**
  * This is a class.
@@ -16,7 +17,7 @@ public class Greeter {
  @param someone the name of a person
  @return greeting string
  */
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
